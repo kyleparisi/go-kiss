c042ea96a070ae5f7c7911511f0d6a7a2873f482
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-08-13 13:15:21 -0400

    Slight modification to the testing setup

--- main_test.go
+++ main_test.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"log"
+	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -17,13 +18,44 @@ func TestHello(t *testing.T) {
 
 // Reference: https://www.calhoun.io/why-cant-i-pass-this-function-as-an-http-handler/
 // Reference: https://blog.questionable.services/article/testing-http-handlers-go/
+// Reference: http://blog.ralch.com/tutorial/design-patterns/golang-adapter/
 func TestHandleLogin(t *testing.T)  {
+	// Create the Request "Object"
+	// Object being a pointer to a value in a memory address (*x => &y => object)
+	// And values are either base types (string, int, etc), structs with properties
+	// that are allocated via make() if of type slices, maps, and channels
 	r, _ := http.NewRequest("POST", "/login", nil)
+	// Convert the interface "http.ResponseWriter" into "Object"
+	// Object being a pointer to a value in a memory address (*x => &y => object)
 	w := httptest.NewRecorder()
-	// func -> HandlerFunc -> Handler
-	handler := http.HandlerFunc(handleLogin)
-	handler.ServeHTTP(w, r)
+	handleLogin(w, r)
 	if w.Code != http.StatusOK {
-		log.Fatalf("Did not receive a 200")
+		t.Fatalf("Not 200 status")
+	}
+}
+
+// If you wanted to make the objects by hand, this is equivalent to the above
+// test.
+func TestHandleLoginDetailed(t *testing.T)  {
+	u,_ := url.Parse("/login")
+	req := &http.Request{
+		Method:     "POST",
+		URL:        u,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     make(http.Header),
+		Body:       nil,
+		Host:       u.Host,
+	}
+	// ResponseRecorder's body is a memory buffer
+	// An actual http.Response body is an io.ReadCloser
+	res := &httptest.ResponseRecorder{
+		Body:      new(bytes.Buffer),
+		Code:      200,
+	}
+	handleLogin(res, req)
+	if res.Code != http.StatusOK {
+		t.Fatalf("Not 200 status")
 	}
 }
