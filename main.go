dc9aab0e264056ec3660a68dc40c836876d1d20b
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-08-14 16:45:50 -0400

    different approach

--- main.go
+++ main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func hello() string {
-	return "Hello world!"
+type Login struct {
+	Email string
+	Password string
+	Remember bool
 }
 
-func handleLogin(w http.ResponseWriter, r *http.Request)  {
-	w.WriteHeader(http.StatusOK)
+type LoginError struct {
+	Errors struct{
+		Email string `json:"email"`
+		Password string `json:"password"`
+	} `json:"errors"`
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params)  {
+	m := Login{}
+	_ = json.NewDecoder(r.Body).Decode(&m)
+	hasEmail := m.Email != ""
+	hasPassword := m.Password != ""
+	// Input validation
+	if !hasEmail || !hasPassword {
+		loginError := LoginError{Errors: struct {
+			Email string `json:"email"`
+			Password string `json:"password"`
+		}{Email: "", Password: ""}}
+		if !hasEmail {
+			loginError.Errors.Email = "Please provide an email address"
+		}
+		if !hasPassword {
+			loginError.Errors.Password = "Please provide a password"
+		}
+		w.WriteHeader(400)
+		response, _ := json.Marshal(loginError)
+		_, _ = w.Write(response)
+		return
+	}
 }
 
 func main() {
-	http.HandleFunc("/login", handleLogin)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		_, _ = fmt.Fprint(writer, "Welcome!\n")
+	})
+	router.POST("/login", handleLogin)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- main_test.go
+++ main_test.go
@@ -1,61 +1,19 @@
 package main
 
 import (
-	"bytes"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
+	"strings"
 	"testing"
+
+	"gotest.tools/assert"
 )
 
-func TestHello(t *testing.T) {
-	expectedStr := "Hello world!"
-	result := hello()
-	if result != expectedStr {
-		t.Fatalf("Expected %s, got %s", expectedStr, result)
-	}
-}
-
-// Reference: https://www.calhoun.io/why-cant-i-pass-this-function-as-an-http-handler/
-// Reference: https://blog.questionable.services/article/testing-http-handlers-go/
-// Reference: http://blog.ralch.com/tutorial/design-patterns/golang-adapter/
 func TestHandleLogin(t *testing.T)  {
-	// Create the Request "Object"
-	// Object being a pointer to a value in a memory address (*x => &y => object)
-	// And values are either base types (string, int, etc), structs with properties
-	// that are allocated via make() if of type slices, maps, and channels
-	r, _ := http.NewRequest("POST", "/login", nil)
-	// Convert the interface "http.ResponseWriter" into "Object"
-	// Object being a pointer to a value in a memory address (*x => &y => object)
-	w := httptest.NewRecorder()
-	handleLogin(w, r)
-	if w.Code != http.StatusOK {
-		t.Fatalf("Not 200 status")
-	}
-}
-
-// If you wanted to make the objects by hand, this is equivalent to the above
-// test.
-func TestHandleLoginDetailed(t *testing.T)  {
-	u,_ := url.Parse("/login")
-	req := &http.Request{
-		Method:     "POST",
-		URL:        u,
-		Proto:      "HTTP/1.1",
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header:     make(http.Header),
-		Body:       nil,
-		Host:       u.Host,
-	}
-	// ResponseRecorder's body is a memory buffer
-	// An actual http.ResponseWriter body is an io.ReadCloser
-	res := &httptest.ResponseRecorder{
-		Body:      new(bytes.Buffer),
-		Code:      200,
-	}
-	handleLogin(res, req)
-	if res.Code != http.StatusOK {
-		t.Fatalf("Not 200 status")
-	}
+	req, _ := http.NewRequest("POST", "/login", nil)
+	res := httptest.NewRecorder()
+	handleLogin(res, req, httprouter.Params{})
+	assert.Assert(t, strings.Contains(res.Body.String(), "Please provide an email"))
+	assert.Assert(t, strings.Contains(res.Body.String(), "Please provide a password"))
 }
